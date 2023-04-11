a8ab196524bf0c7c9d161f6c39b7d207bfa249f1
Juan Quiceno <123400571+JuanQuicenoQA@users.noreply.github.com>
2023-04-11 11:33:42 -0500

    Add POST Call with Axios

--- test/specs/login2.js
+++ test/specs/login2.js
@@ -1,5 +1,6 @@
 import LoginPage from '../page-objects/login-page';
 import Random from '../utils/api-calls';
+import AllureReporter from '@wdio/allure-reporter';
 
 before(async () => {
 });
@@ -19,8 +20,11 @@ describe('API Feature', () => {
     it('API Call', async () => {
         AllureReporter.addStory("Successfully API Call");
         const randomJoke = await browser.call(() => Random.getRandomJoke());
+        const newPet = await browser.call(() => Random.postNewPet());
 
-        console.log("***** " + randomJoke.data.id);
-        console.log("***** " + randomJoke.data.value);
+        console.log("***** Id Joke " + randomJoke.data.id);
+        console.log("***** Value Joke" + randomJoke.data.value);
+        console.log("***** Id Pet " + newPet.data.id);
+        console.log("***** Name Pet " + newPet.data.name);
     });
 });
--- test/utils/api-calls.js
+++ test/utils/api-calls.js
@@ -1,8 +1,21 @@
 const axios = require('axios');
+import postNewPet from '../data/postNewPet.json';
 
 class RandomAPI{
     async getRandomJoke(){
         const response = await axios.get('https://api.chucknorris.io/jokes/random');
+
+        return response;
+    }
+
+    async postNewPet(){
+        const response = await axios.post('https://petstore.swagger.io/v2/pet', postNewPet, {
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+    );
 
         return response;
     }
